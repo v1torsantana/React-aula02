69db07b4f02169f447fc28f63111d6f87996cb00
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 22:45:09 -0300

    commit final dia 23/08

--- src/cabecalho.jsx
+++ src/cabecalho.jsx
@@ -1,9 +1,17 @@
 import {} from 'react'
+import Imagem from './assets/calleri.jpg'
 
 function Cabecalho(){
+
+    const aluno ='aluno';
+    const curso = 'fiap';
     return (
     <>
     <h1>Cabeçalho</h1>
+
+    <img src={Imagem} alt="Toca no Calleri que é gol" width="300px" />
+    <p> {aluno}</p>
+    <p> {curso}</p>
     </>
 
     )
